2028da620bae9433e5daaf17466674588d949ac0
Bao Tran <baopei05@gmail.com>
2026-09-13 13:04:26 -0400

    Introduce multiple threads

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <vector>
 #include <unordered_map>
+#include <thread>
+#include <functional>
 
 // Supported commands:
 //  get <key> : Retrieve the value with the corresponding key.
@@ -7,61 +10,72 @@
 //      Otherwise, put the key-value pair to have such new value.
 //  delete <key> : Delete such key-value pair if exists. Otherwise, do nothing.
 
-// Note: <key> is of type 32-bit unsigned int.
-//       <value> is of type 32-bit int.
+// Note: <key> is of type 16-bit unsigned int.
+//       <value> is of type int.
 
 
-class Concurrent_KV {
-    std::unordered_map<int, int> m;
+int _get(std::unordered_map<u_int16_t, int> &kv, u_int16_t key) {
+    return kv[key];
+}
 
-  public:
-    int _get(int key);
-    void _set(int key, int value);
-    void _delete(int key);
+void _set(std::unordered_map<u_int16_t, int> &kv, u_int16_t key, int value) {
+    kv[key] = value;
+}
+
+void _delete(std::unordered_map<u_int16_t, int> &kv, u_int16_t key) {
+    kv.erase(key);
+}
+
+enum class Command {
+    SET, GET, DELETE
 };
 
-int Concurrent_KV::_get(int key) {
-    return m[key];
-}
+struct Operation {
+    Command cmd;
+    u_int16_t key;
+    int value;
+};
 
-void Concurrent_KV::_set(int key, int value) {
-    m[key] = value;
-}
-
-void Concurrent_KV::_delete(int key) {
-    m.erase(key);
-}
 
 int main() {
-    Concurrent_KV kv;
+    std::unordered_map<u_int16_t, int> kv;
+    std::vector<Operation> operations;
 
     std::string command;
     while (std::cin >> command) {
         if (command == "get") {
-            int key;
+            u_int16_t key;
             std::cin >> key;
-
-            int value =kv._get(key);
-            std::cout << value << std::endl;
+            operations.push_back(Operation {Command::GET, key, 0});
         }
         else if (command == "set") {
-            int key;
+            u_int16_t key;
             int value;
             std::cin >> key >> value;
-
-            kv._set(key, value);
+            operations.push_back(Operation {Command::SET, key, value});
         }
         else if (command == "delete") {
-            int key;
+            u_int16_t key;
             std::cin >> key;
-
-            kv._delete(key);
+            operations.push_back(Operation {Command::DELETE, key, 0});
         }
         else {
             std::cout << "Invalid command. Please try again..." << std::endl;
         }
     }
 
+    std::vector<std::thread> threads;
+    for (Operation &op : operations) {
+        if (op.cmd == Command::GET) {
+            threads.push_back(std::thread (&_get, std::ref(kv), op.key));
+        }
+        else if (op.cmd == Command::SET) {
+            threads.push_back(std::thread (&_set, std::ref(kv), op.key, op.value));
+        }
+        else if (op.cmd == Command::DELETE) {
+            threads.push_back(std::thread (&_delete, std::ref(kv), op.key));
+        }
+    }
 
     return 0;
 }
