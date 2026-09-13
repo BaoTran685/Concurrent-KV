6e55e57719ccb80f2246ca720ab030d471cb20d3
Bao Tran <baopei05@gmail.com>
2026-09-13 15:18:09 -0400

    Concurrecy for GET commands only. Mutex added

--- src/main.cc
+++ src/main.cc
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <queue>
 #include <thread>
-#include <functional>
+#include <future>
 
 // Supported commands:
 //  get <key> : Retrieve the value with the corresponding key.
@@ -13,16 +14,29 @@
 // Note: <key> is of type int.
 //       <value> is of type int.
 
+// Concurrency: All GET tasks can be executed concurrently.
+//  SET and DELETE tasks needed to be executed sequentially.
 
-int _get(std::unordered_map<int, int> &kv, int key) {
-    return kv[key];
+
+std::unordered_map<int, int> kv;
+std::mutex kv_mutex;
+
+int _get(int key) {
+    auto it = kv.find(key);
+    if (it == kv.end()) {
+        // key doesn't exist
+        return 0;
+    }
+    return it->second;
 }
 
-void _set(std::unordered_map<int, int> &kv, int key, int value) {
+void _set(int key, int value) {
+    std::lock_guard<std::mutex> lock(kv_mutex);
     kv[key] = value;
 }
 
-void _delete(std::unordered_map<int, int> &kv, int key) {
+void _delete(int key) {
+    std::lock_guard<std::mutex> lock(kv_mutex);
     kv.erase(key);
 }
 
@@ -30,7 +44,7 @@ enum class Command {
     SET, GET, DELETE
 };
 
-struct Operation {
+struct Task {
     Command cmd;
     int key;
     int value;
@@ -38,47 +52,74 @@ struct Operation {
 
 
 int main() {
-    std::unordered_map<int, int> kv;
-    std::vector<Operation> operations;
+    std::queue<Task> task_queue;
 
     std::string command;
     while (std::cin >> command) {
         if (command == "get") {
             int key;
             std::cin >> key;
-            operations.push_back(Operation {Command::GET, key, 0});
+            task_queue.push(Task {Command::GET, key, 0});
         }
         else if (command == "set") {
             int key;
             int value;
             std::cin >> key >> value;
-            operations.push_back(Operation {Command::SET, key, value});
+            task_queue.push(Task {Command::SET, key, value});
         }
         else if (command == "delete") {
             int key;
             std::cin >> key;
-            operations.push_back(Operation {Command::DELETE, key, 0});
+            task_queue.push(Task {Command::DELETE, key, 0});
         }
         else {
             std::cout << "Invalid command. Please try again..." << std::endl;
         }
     }
 
-    std::vector<std::thread> threads;
-    for (Operation &op : operations) {
-        if (op.cmd == Command::GET) {
-            threads.push_back(std::thread (&_get, std::ref(kv), op.key));
-        }
-        else if (op.cmd == Command::SET) {
-            threads.push_back(std::thread (&_set, std::ref(kv), op.key, op.value));
-        }
-        else if (op.cmd == Command::DELETE) {
-            threads.push_back(std::thread (&_delete, std::ref(kv), op.key));
-        }
-    }
 
-    for (std::thread &t : threads) {
-        t.join();
+    while (!task_queue.empty()) {
+
+        // Creating threads
+        std::vector<std::thread> thread_vector;
+        std::vector<std::future<int>> result_vector;
+        if (task_queue.front().cmd == Command::GET) {
+            while (!task_queue.empty() && task_queue.front().cmd == Command::GET) {
+
+                std::packaged_task<int(int)> task(_get);
+                result_vector.push_back(task.get_future());
+
+                int key = task_queue.front().key;
+                thread_vector.push_back(std::thread(std::move(task), key));
+
+                task_queue.pop();
+            }
+        }
+        else if (task_queue.front().cmd == Command::SET) {
+            int key = task_queue.front().key;
+            int value = task_queue.front().value;
+            thread_vector.push_back(std::thread(&_set, key, value));
+            task_queue.pop();
+        }
+        else if (task_queue.front().cmd == Command::DELETE) {
+            int key = task_queue.front().key;
+            thread_vector.push_back(std::thread(&_delete, key));
+            task_queue.pop();
+        }
+        else {
+            throw std::runtime_error("Invalid Command.");
+        }
+
+        // Joining threads
+        for (std::thread &t : thread_vector) {
+            t.join();
+        }
+
+        // Print results
+        for (std::future<int> &r : result_vector) {
+            std::cout << r.get() << std::endl;
+        }
+
     }
 
     return 0;
